37a2b876c71dfbb990e146d932964c513c4ca4f6
Justin Blau <justin.blau@oracle.com>
2019-07-03 18:36:07 +0000

    fixed bug

--- cuda.py
+++ cuda.py
@@ -1,4 +1,26 @@
+import numpy as np
 from numba import vectorize
+
+a = np.array([1, 2, 3, 4])
+b = np.array([10, 20, 30, 40])
+
+np.add(a, b)
+
+np.add(a, 100)
+
+c = np.arange(4*4).reshape((4,4))
+print('c:', c)
+
+np.add(b, c)
+
+
+
+b_col = b[:, np.newaxis]
+b_col
+
+np.add(b_col, c)
+
+
 
 @vectorize(['int64(int64, int64)'], target='cuda')
 def add_ufunc(x, y):
