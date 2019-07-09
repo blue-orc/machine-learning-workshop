ff62133c798b4e233cfa2961061f4a6b97a60572
Justin Blau <justin.blau@oracle.com>
2019-07-09 15:53:11 -0400

    remove print

--- generate-data.py
+++ generate-data.py
@@ -35,7 +35,7 @@ def logistic_regression(features, target, num_steps, learning_rate, add_intercep
         # Print log-likelihood every so often
         if step % 1000 == 0:
             result = log_likelihood(features, target, weights)
-            print('Step ' + str(step) + ': ' + str(result))
+            #print('Step ' + str(step) + ': ' + str(result))
         
     return weights
 
