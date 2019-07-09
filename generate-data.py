7a52ced5ab4688f1cb5e5e34d2fe01913fcc5c53
Justin Blau <justin.blau@oracle.com>
2019-07-09 16:45:33 -0400

    return to vectorize

--- generate-data.py
+++ generate-data.py
@@ -11,7 +11,7 @@ def log_likelihood(features, target, weights):
     ll = np.sum( target*scores - np.log(1 + np.exp(scores)) )
     return ll
 
-@jit
+@vectorize(['float32(float64, float32, int32, float32, boolean)'], target='cuda')
 def logistic_regression(features, target, num_steps, learning_rate, add_intercept = False):
     if add_intercept:
         intercept = np.ones((features.shape[0], 1))
